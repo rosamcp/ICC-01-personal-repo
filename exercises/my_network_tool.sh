df553e3c33905ac0ec5143242bcd3ca9f4346fee
fred <frederico.alves.rosa.96@gmail.com>
2024-11-11 16:09:10 +0000

    Updated my_network_tool.sh

--- exercises/my_network_tool.sh
+++ exercises/my_network_tool.sh
@@ -13,24 +13,29 @@ read input
 
 while [ $input != 6 ];
 do 
-    case $input in 
+    case $input in
+        # Network Interface Information     
         "1")
             echo Network Interface Information:
             ifconfig
         ;;
+        # Ping a Host
         "2")
-            echo Which address do you want to ping?
+            echo Which host do you want to ping?
             read input
             ping -c 2 $input
         ;;
+        # Port Scan with Nmap
         "3")
             echo Which host do you want to port scan?
             read input
             nmap -p0- $input
         ;;
+        # Display Routing Table
         "4")
             route
         ;;
+        # Traceroute to Host
         "5")
             echo Which host do you want to traceroute?
             read input
@@ -39,7 +44,7 @@ do
     esac
     echo " 
     What do you want to do next?
-    
+
 1) Check Network Interface Information
 2) Ping a Host
 3) Port Scan with Nmap
@@ -47,6 +52,7 @@ do
 5) Traceroute to Host
 6) Exit"
     read input
+    # Exit
     if [ $input = 6 ]; then 
         break
     fi
