fe2508e3e62d4c6cecc12d1a9321412ca9b3f3ba
fred <frederico.alves.rosa.96@gmail.com>
2024-12-03 00:04:34 +0000

    Added first_python_script.py

---
+++ exercises/python/first_python_script.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+option = None
+
+def menu () :
+    print (" **Menu**")
+    print (" 1) Read and display names")
+    print (" 2) Add a new name")
+    print (" \n 3) Exit")
+
+def read_and_display():
+    with open ("names.txt", "r") as file:
+        for name in file:
+            print (name.strip())
+
+def write_name():
+    with open ("names.txt", "a") as file:
+        file.write("\n" + input ("Enter a new name: "))
+
+while option != "3" :
+    menu()
+    option = input()
+    if option == "1":
+        read_and_display()
+    if option == "2":
+        write_name()
